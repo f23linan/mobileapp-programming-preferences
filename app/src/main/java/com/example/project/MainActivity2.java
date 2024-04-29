a55b747177e95252ec46661d643715db4dea5ed8
f23linan <f23linan@student.his.se>
2024-04-29 11:23:02 +0200

    Edittexts are now saved when submitted

--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -2,6 +2,8 @@ package com.example.project;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,12 +18,8 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
         TextView textview2 = findViewById(R.id.prefButton);
-
         myPreferenceRef = getPreferences(MODE_PRIVATE);
         myPreferenceEditor = myPreferenceRef.edit();
-        TextView prefTextRef=new TextView(this);
-        prefTextRef=(TextView)findViewById(R.id.prefText);
-        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -29,5 +27,29 @@ public class MainActivity2 extends AppCompatActivity {
             int number = extras.getInt("number");
             textview2.setText(name);
         }
+
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
     }
+
+    public void savePref(View v){
+        // Get the text
+        EditText newPrefText=new EditText(this);
+        newPrefText=(EditText)findViewById(R.id.settingseditview);
+
+        // Store the new preference
+        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+
+        // Display the new preference
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
+        // Clear the EditText
+        newPrefText.setText("");
+    }
+
 }
